71f77c2f44f759a081464a0c35144db7b410006d
Efim Nikitin <e.nikitin1@g.nsu.ru>
2023-04-04 15:56:14 +0700

    Add main class

---
+++ src/main/java/com/helpet/service/newsfeed/NewsFeedServiceApplication.java
@@ -0,0 +1,11 @@
+package com.helpet.service.newsfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFeedServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NewsFeedServiceApplication.class, args);
+    }
+}
